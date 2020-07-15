166d7077ac9229b5646ddd4e79af9638312cd097
Vivvianne <ndiahav@gmail.com>
2020-07-15 18:13:34 +0300

    added the Post class in the models

--- Mynotes/models.py
+++ Mynotes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    organization = models.CharField(max_length=100)
+    agenda = models.CharField(max_length=300)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    attendance = models.TextField()
+    content =  models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.organization
 
 
