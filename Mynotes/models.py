0c1db6fe8645a84a3c6d6ff4508daeda902d33da
Vivvianne <ndiahav@gmail.com>
2020-07-17 16:20:05 +0300

    used class based views to add create, update and delete posts/notes in mynotes app

--- Mynotes/models.py
+++ Mynotes/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     organization = models.CharField(max_length=100)
@@ -12,5 +14,8 @@ class Post(models.Model):
     
     def __str__(self):
         return self.organization
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
 
 
